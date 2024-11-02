250c78c5ca66f30c9d3532709d2e97390362dc99
jessicapeck <jess@pecksplace.com>
2024-11-02 15:51:45 +0000

    Initialise pins

--- server/roboto_server.py
+++ server/roboto_server.py
@@ -1,14 +1,43 @@
+import RPi.GPIO as GPIO
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Hello world'
+    return 'Welcome to MR ROBOTO'
 
-@app.route('/unit1')
-def unit1():
-    return 'Raspberry Pie!'
+# big unit has big_unit_1 and big_unit_2
+@app.route('big_unit_1')
+def big_unit_1():
+    pass
+
+@app.route('big_unit_2')
+def big_unit_2():
+    pass
+
+# little unit has little_unit_1 and little_unit_2
+@app.route('little_unit_1')
+def big_unit_1():
+    pass
+
+@app.route('little_unit_2')
+def big_unit_2():
+    pass
+
+@app.route('initialise')
+def initialise():
+    GPIO.setmode(GPIO.BCM)
+
+    GPIO.setup(2, GPIO.out, initial=1)  # big unit channel A up
+    GPIO.setup(3, GPIO.out, initial=1)  # big unit channel A down
+    GPIO.setup(14, GPIO.out, initial=1)  # big unit channel B up
+    GPIO.setup(15, GPIO.out, initial=1)  # big unit channel B down
+
+    GPIO.setup(17, GPIO.out, initial=1)  # little unit channel A up
+    GPIO.setup(27, GPIO.out, initial=1)  # little unit channel A down
+    GPIO.setup(23, GPIO.out, initial=1)  # little unit channel B up
+    GPIO.setup(24, GPIO.out, initial=1)  # little unit channel B down
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
